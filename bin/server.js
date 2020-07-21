526d62e55527ee6e372a862f38f402f81423b5a5
Drewry Pope <drewrypope@gmail.com>
2020-07-20 22:36:00 -0500

    add standard template

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env node
-/* Author: Drewry Pope
- * Any copyright is dedicated to the Public Domain.
- * https://creativecommons.org/publicdomain/zero/1.0/ */
-import { sleepWithLog } from 'sleep-atomic'
-sleepWithLog((process.argv[2] || process.env.SLEEP_SECONDS || 10) * 1000);
+/* Copyright 2020 Drewry Pope
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import bot from 'twitter-reply-bot'
+bot.startSync();
