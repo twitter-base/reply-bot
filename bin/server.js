9a1bbae15eae26d57ee0eef964646a44e67b339b
Drewry Pope <drewrypope@gmail.com>
2020-07-20 23:39:53 -0500

    small changes, working code

--- bin/server.js
+++ bin/server.js
@@ -2,5 +2,5 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import bot from 'twitter-reply-bot'
+import bot from '../lib/index.js'
 bot.startSync();
